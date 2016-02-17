d82c0594202fb230ea1ef9aa69f06fdb88a3e16b
tanchao <imtanchao@163.com>
2016-02-17 14:54:47 +0800

    初始化

---
+++ CollectionViewDemo/CollectionViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionViewDemo
+//
+//  Created by tanchao on 16/2/17.
+//  Copyright © 2016年 谈超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CollectionViewDemo/CollectionViewDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CollectionViewDemo
+//
+//  Created by tanchao on 16/2/17.
+//  Copyright © 2016年 谈超. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
